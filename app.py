77b8279ef9f1c8b7bd7b339fe88865a370baf198
Susanna D <dongsusanna@gmail.com>
2019-01-06 18:57:43 -0500

    Refine a bit of equipment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 import json
+import sys
 
 from base import Jsonable, PlayerBase
 from race import HillDwarf, RockGnome, Human
@@ -7,9 +8,28 @@ from pclass import RangerFactory
 from background import Criminal
 from pc import PlayerCharacter
 
+import equipment
+
 from sheet import CharacterSheetGenerator
 
+"""
+Testing playground for D&D Infinity.
+"""
 def main():
+    if len(sys.argv) < 2:
+        print('put in argument pls')
+        print('"pc" for testing pc')
+        print('"stuff" for testing equipment')
+    elif sys.argv[1] == 'pc':
+        test_pc()
+    elif sys.argv[1] == 'stuff':
+        test_stuff()
+    else:
+        print('not sure what you put in')
+        print('try something else')
+    print('bye')
+
+def test_pc():
     base = PlayerBase("Dorian Sapbleden", 16, 10, 14, 12, 14, 8, level=2)
     race = HillDwarf({})
     classes = [
@@ -38,6 +58,14 @@ def main():
     #csheet = CharacterSheetGenerator().generate_character_sheet(pc)
     #print(json.dumps(csheet, indent=4))
 
+def test_stuff():
+    backpack = equipment.Backpack(gold_pieces=15)
+    print(backpack)
+    backpack.add_item(equipment.Weapon('dagger', '1d4 piercing', 200, 1, ['finesse', 'light', 'thrown (range 20/60)'])
+)
+    backpack.add_item(equipment.Item('crowbar'))
+    backpack.add_item(equipment.Item('clothes', description='A set of dark common clothes including a hood.'))
+    print(backpack)
 
 if __name__ == '__main__':
     main()
--- equipment.py
+++ equipment.py
@@ -10,23 +10,89 @@ The 'trasient' feature will likely is delegated to
 a specific service.
 """
 class Backpack(Jsonable):
-    def __init__(self):
-	self.copper_pieces = 0
-	self.silver_pieces = 0
-	self.gold_pieces = 0
-	self.platnium_pieces = 0
+    def __init__(self,
+                 copper_pieces=0,
+                 silver_pieces=0,
+                 gold_pieces=0,
+                 platnium_pieces=0,
+                 items=[]):
+        self.copper_pieces = copper_pieces
+        self.silver_pieces = silver_pieces
+        self.gold_pieces = gold_pieces
+        self.platnium_pieces = platnium_pieces
 
-	self.items = []
+        self.items = items
+
+    def add_item(self, item):
+        self.items.append(item)
 
     def __json__(self):
-	j = {
-	    'money': {
-		'CP': self.copper_pieces,
-		'SP': self.silver_pieces,
-		'GP': self.gold_pieces,
-		'PP': self.platnium_pieces,
-	    },
-	    'items': self.items,
-	}
-	return j
+        items_p = []
+        for i in self.items:
+            items_p.append(i.__json__())
+        j = {
+            'money': {
+            'CP': self.copper_pieces,
+            'SP': self.silver_pieces,
+            'GP': self.gold_pieces,
+            'PP': self.platnium_pieces,
+            },
+            'items': items_p,
+        }
+        return j
 
+"""
+An item in D&D.
+The price will assume that it is in copper pieces (CP),
+since that is the lowest denomination of money.
+Weight is in pounds (lbs).
+"""
+class Item(Jsonable):
+    def __init__(self, name, price=0, weight=0, description=None):
+        self.name = name
+        self.description = description
+        self.price = 0
+        self.weight = 0
+
+    def __json__(self):
+        j = {
+            'name': self.name,
+        }
+        if self.description:
+            j['description'] = self.description
+        if self.price > 0:
+            j['price'] = self.price
+        if self.weight > 0:
+            j['weight'] = self.weight
+        return j
+
+"""
+Weapon in D&D.
+Weapons are simple or martial weapons.
+Weapons can have many different properties and damage.
+"""
+class Weapon(Item):
+    def __init__(self, name, damage, price, weight, properties):
+        super(Weapon, self).__init__(name, price, weight)
+        self.damage = damage
+        self.properties = properties
+
+    def __json__(self):
+        j = {
+            **super(Weapon, self).__json__(),
+            'damage': self.damage,
+            'properties': self.properties,
+        }
+        return j
+
+"""
+Armor in D&D.
+Armor are light, medium, or heavy, or a shield.
+They have different prerequisites and effects.
+"""
+class Armor(Item):
+    def __init__(self, name, price, weight, armor_class, strength, stealth):
+        super(Armor, self).__init__(name, price, weight)
+        self.armor_class = armor_class
+        self.strength = strength
+        self.stealth = stealth
