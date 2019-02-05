e8a955928e2e47aa1846f33978d410965b520170
Susanna <dongsusanna@gmail.com>
2019-02-04 19:50:14 -0500

    Clean up unit tests for race

--- tests/entity/character/test_race.py
+++ tests/entity/character/test_race.py
@@ -6,17 +6,42 @@ from ddddd.entity.character import race, trait
 
 class TestRace(unittest.TestCase):
     def setUp(self):
-        self.dorian = race.Race(name='race',
-                                asi={},
-                                size='medium',
-                                speed=25,
-                                languages=None,
-                                traits=None)
+        traits = [
+            trait.Darkvision(range=60),
+            trait.ToolProficiency(name='Tool Proficiency',
+                                  proficiencies=['brewers_kit'])
+        ]
+        self.race = race.Race(name='race',
+                              asi={},
+                              size='medium',
+                              speed=25,
+                              languages=None,
+                              traits=traits)
 
     def test_str_movement_multiplier(self):
-        result = self.dorian.str_movement_multiplier
+        result = self.race.str_movement_multiplier
         self.assertEqual(result, 1)
 
+    def test_skills(self):
+        result = self.race.skills
+        self.assertEqual(result, [])
+
+    def test_proficiencies(self):
+        result = self.race.proficiencies
+        self.assertEqual(len(result), 1)
+
+        prof_result = result.get('Tool Proficiency')
+        self.assertTrue(isinstance(prof_result, trait.ToolProficiency))
+
+    def test_verify(self):
+        self.assertEqual(self.race.verify(), {})
+
+    def test_required(self):
+        self.assertEqual(self.race.required(), {})
+
+###########################
+# DWARF
+###########################
 
 class TestDwarfRace(unittest.TestCase):
     def setUp(self):
@@ -24,41 +49,33 @@ class TestDwarfRace(unittest.TestCase):
             trait.ToolProficiency(name='Tool Proficiency',
                                   proficiencies=['brewers_kit'])
         ]
-        self.dorian = race.Dwarf(asi={},
-                                 traits=tool_prof)
+        self.dwarf = race.Dwarf(asi={},
+                                traits=tool_prof)
 
     def test_default_traits(self):
-        self.assertEqual(2, self.dorian.asi['CON'].score_increase)
+        self.assertEqual(2, self.dwarf.asi['CON'].score_increase)
 
         expected_trait_names = {'Darkvision',
                                 'Dwarven Combat Training',
                                 'Dwarven Resilience',
                                 'Stonecunning',
                                 'Tool Proficiency'}
-        result = set(map(lambda t: t.name, self.dorian.traits))
+
+        result = set(map(lambda t: t.name, self.dwarf.traits))
         self.assertEqual(expected_trait_names, result)
 
+        self.assertTrue(isinstance(self.dwarf.languages, trait.LanguagesKnown))
+
     def test_proficiencies(self):
-        prof = self.dorian.proficiencies
+        prof = self.dwarf.proficiencies
 
         self.assertEqual(4, len(prof[base.WEAPON_PROFICIENCY].proficiencies))
 
-        tool_prof = list(filter(lambda p: isinstance(p, trait.ToolProficiency), prof))
         self.assertEqual('brewers_kit', prof[base.TOOL_PROFICIENCY].proficiencies[0])
 
     def test_required(self):
-        req = self.dorian.required()
+        req = self.dwarf.required()
         self.assertEqual(1, len(req.keys()))
-
-    def test_verify_good(self):
-        self.assertEqual({}, self.dorian.verify())
-
-    # def test_verify_bad(self):
-    #     no_tool = race.Dwarf(asi={}, traits=None)
-    #     self.assertRaises(ValueError, no_tool.verify)
-    #
-    #     invalid_tool = race.Dwarf(asi={}, traits={'tool_proficiency': {'tools': []}})
-    #     self.assertRaises(ValueError, invalid_tool.verify)
 
 
 class TestHillDwarfRace(unittest.TestCase):
@@ -67,11 +84,11 @@ class TestHillDwarfRace(unittest.TestCase):
             trait.ToolProficiency(name='Tool Proficiency',
                                   proficiencies=['brewers_kit'])
         ]
-        self.dorian = race.HillDwarf(traits=tool_prof)
+        self.hill_dwarf = race.HillDwarf(traits=tool_prof)
 
     def test_default_traits(self):
-        self.assertEqual(2, self.dorian.asi['CON'].score_increase)
-        self.assertEqual(1, self.dorian.asi['WIS'].score_increase)
+        self.assertEqual(2, self.hill_dwarf.asi['CON'].score_increase)
+        self.assertEqual(1, self.hill_dwarf.asi['WIS'].score_increase)
 
         expected_trait_names = {'Darkvision',
                                 'Dwarven Combat Training',
@@ -79,5 +96,73 @@ class TestHillDwarfRace(unittest.TestCase):
                                 'Stonecunning',
                                 'Tool Proficiency',
                                 'Dwarven Toughness'}
-        result = set(map(lambda t: t.name, self.dorian.traits))
+        result = set(map(lambda t: t.name, self.hill_dwarf.traits))
         self.assertEqual(expected_trait_names, result)
+
+    def test_base_race(self):
+        self.assertEqual(self.hill_dwarf.base_race, 'Dwarf')
+
+
+###########################
+# GNOME
+###########################
+
+class TestGnomeRace(unittest.TestCase):
+    def setUp(self):
+        self.gnome = race.Gnome(asi={},
+                                traits=[])
+
+    def test_default_traits(self):
+        self.assertEqual(2, self.gnome.asi['INT'].score_increase)
+
+        expected_trait_names = {'Darkvision',
+                                'Gnome Cunning'}
+
+        result = set(map(lambda t: t.name, self.gnome.traits))
+        self.assertEqual(expected_trait_names, result)
+
+        self.assertTrue(isinstance(self.gnome.languages, trait.LanguagesKnown))
+
+    def test_proficiencies(self):
+        self.assertEqual(len(self.gnome.proficiencies), 0)
+
+
+class TestRockGnomeRace(unittest.TestCase):
+    def setUp(self):
+        self.rock_gnome = race.RockGnome()
+
+    def test_default_traits(self):
+        self.assertEqual(2, self.rock_gnome.asi['INT'].score_increase)
+        self.assertEqual(1, self.rock_gnome.asi['CON'].score_increase)
+
+        expected_trait_names = {'Darkvision',
+                                'Gnome Cunning',
+                                "Artificer's Lore",
+                                'Tinker'}
+        result = set(map(lambda t: t.name, self.rock_gnome.traits))
+        self.assertEqual(expected_trait_names, result)
+
+    def test_base_race(self):
+        self.assertEqual(self.rock_gnome.base_race, 'Gnome')
+
+
+###########################
+# TIEFLING
+###########################
+
+class TestTieflingRace(unittest.TestCase):
+    def setUp(self):
+        self.tiefling = race.Tiefling()
+
+    def test_default_traits(self):
+        self.assertEqual(2, self.tiefling.asi['CHA'].score_increase)
+        self.assertEqual(1, self.tiefling.asi['INT'].score_increase)
+
+        expected_trait_names = {'Darkvision',
+                                'Hellish Resistance',
+                                'Infernal Legacy'}
+        result = set(map(lambda t: t.name, self.tiefling.traits))
+        self.assertEqual(expected_trait_names, result)
+
+    def test_base_race(self):
+        self.assertEqual(self.tiefling.base_race, 'Tiefling')
