a2822e06cf8b2c2ace0e7916bc6dbcd00d36390d
Susanna <dongsusanna@gmail.com>
2019-02-20 19:26:29 -0500

    Add stubs for the rest of the classes that need unit tests

---
+++ tests/entity/character/test_trait.py
@@ -0,0 +1,9 @@
+import unittest
+
+from ddddd.entity import base
+from ddddd.entity.character import trait
+
+
+class TestTrait(unittest.TestCase):
+    def setUp(self):
+        pass
---
+++ tests/entity/character/vocations/test_ranger.py
@@ -0,0 +1,9 @@
+import unittest
+
+from ddddd.entity import base
+from ddddd.entity.character.vocations import ranger
+
+
+class TestRanger(unittest.TestCase):
+    def setUp(self):
+        pass
