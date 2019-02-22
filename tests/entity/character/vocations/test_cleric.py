49c15c46a57c11f520d2be25ac47224808b88148
Susanna <dongsusanna@gmail.com>
2019-02-21 20:08:53 -0500

    Add unit tests for vocations
    Note that I'm still stubbing out the unit tests for spellcasting

--- tests/entity/character/vocations/test_cleric.py
+++ tests/entity/character/vocations/test_cleric.py
@@ -1,9 +1,78 @@
 import unittest
 
 from ddddd.entity import base
-from ddddd.entity.character.vocations import cleric
+from ddddd.entity.base import Skills
+from ddddd.entity.character import feature, spells
+from ddddd.entity.character.vocations import ranger, cleric
 
 
-class TestCleric(unittest.TestCase):
+class TestClericLevel1(unittest.TestCase):
     def setUp(self):
+        class_languages = feature.LanguagesKnown(languages=[base.Languages.DRACONIC, base.Languages.DWARVISH])
+        class_cantrips = [spells.SACRED_FLAME, spells.GUIDANCE, spells.SPARE_THE_DYING]
+        self.cleric = cleric.Cleric(skill_proficiencies=[Skills.INSIGHT, Skills.RELIGION, Skills.ARCANA, Skills.PERSUASION],
+                                    languages=class_languages, cantrips=class_cantrips)
+
+    def test_name(self):
+        self.assertEqual('Cleric', self.cleric.name)
+
+    def test_level(self):
+        self.assertEqual(1, self.cleric.level)
+
+    def test_hit_die(self):
+        self.assertEqual(8, self.cleric.hit_die)
+
+    def test_proficiencies(self):
+        self.assertEqual(2, len(self.cleric.proficiencies))
+
+    def test_saving_throws(self):
+        self.assertEqual(['WIS', 'CHA'], self.cleric.saving_throws)
+
+    def test_features(self):
+        self.assertEqual(3, len(self.cleric.features))
+
+    def test_spellcasting(self):
+        # self.assertEqual(None, self.cleric.spellcasting)
         pass
+
+    def test_asi(self):
+        self.assertEqual({}, self.cleric.asi)
+
+
+class TestClericLevel4(unittest.TestCase):
+    def setUp(self):
+        class_languages = feature.LanguagesKnown(languages=[base.Languages.DRACONIC, base.Languages.DWARVISH])
+        class_cantrips = [spells.SACRED_FLAME, spells.GUIDANCE, spells.SPARE_THE_DYING]
+        self.cleric = cleric.Cleric(skill_proficiencies=[Skills.INSIGHT, Skills.RELIGION, Skills.ARCANA, Skills.PERSUASION],
+                                    languages=class_languages, cantrips=class_cantrips)
+
+        self.cleric.level_to(level=4,
+                             cantrip_4=spells.WORD_OF_RADIANCE,
+                             ability_score_increase_4={
+                                 base.AbilityScore.WIS: base.AbilityScoreIncrease(base.AbilityScore.WIS, 2),
+                             })
+
+    def test_name(self):
+        self.assertEqual('Cleric', self.cleric.name)
+
+    def test_level(self):
+        self.assertEqual(4, self.cleric.level)
+
+    def test_hit_die(self):
+        self.assertEqual(8, self.cleric.hit_die)
+
+    def test_proficiencies(self):
+        self.assertEqual(2, len(self.cleric.proficiencies))
+
+    def test_saving_throws(self):
+        self.assertEqual(['WIS', 'CHA'], self.cleric.saving_throws)
+
+    def test_features(self):
+        self.assertEqual(6, len(self.cleric.features))
+
+    def test_spellcasting(self):
+        # self.assertTrue(self.cleric.spellcasting is None)
+        pass
+
+    def test_asi(self):
+        self.assertEqual({'WIS'}, set(self.cleric.asi.keys()))
--- tests/entity/character/vocations/test_ranger.py
+++ tests/entity/character/vocations/test_ranger.py
@@ -1,9 +1,75 @@
 import unittest
 
 from ddddd.entity import base
+from ddddd.entity.base import Skills
 from ddddd.entity.character.vocations import ranger
 
 
-class TestRanger(unittest.TestCase):
+class TestRangerLevel1(unittest.TestCase):
     def setUp(self):
-        pass
+        self.ranger = ranger.Ranger(skill_proficiencies=[Skills.ATHLETICS, Skills.ANIMAL_HANDLING, Skills.SURVIVAL],
+                                    favored_enemy='plants',
+                                    languages='elvish',
+                                    favored_terrain='forest')
+
+    def test_name(self):
+        self.assertEqual('Ranger', self.ranger.name)
+
+    def test_level(self):
+        self.assertEqual(1, self.ranger.level)
+
+    def test_hit_die(self):
+        self.assertEqual(10, self.ranger.hit_die)
+
+    def test_proficiencies(self):
+        self.assertEqual(2, len(self.ranger.proficiencies))
+
+    def test_saving_throws(self):
+        self.assertEqual(['STR', 'DEX'], self.ranger.saving_throws)
+
+    def test_features(self):
+        self.assertEqual(3, len(self.ranger.features))
+
+    def test_spellcasting(self):
+        self.assertEqual(None, self.ranger.spellcasting)
+
+    def test_asi(self):
+        self.assertEqual({}, self.ranger.asi)
+
+
+class TestRangerLevel4(unittest.TestCase):
+    def setUp(self):
+        self.ranger = ranger.Ranger(skill_proficiencies=[Skills.ATHLETICS, Skills.ANIMAL_HANDLING, Skills.SURVIVAL],
+                                    favored_enemy='plants',
+                                    languages='elvish',
+                                    favored_terrain='forest')
+        self.ranger.level_to(level=4,
+                             fighting_style='two_weapon_fighting',
+                             archetype_feature='colossus_slayer',
+                             ability_score_increase_4={
+                                 base.AbilityScore.STR: base.AbilityScoreIncrease(base.AbilityScore.STR, 2),
+                             })
+
+    def test_name(self):
+        self.assertEqual('Ranger', self.ranger.name)
+
+    def test_level(self):
+        self.assertEqual(4, self.ranger.level)
+
+    def test_hit_die(self):
+        self.assertEqual(10, self.ranger.hit_die)
+
+    def test_proficiencies(self):
+        self.assertEqual(2, len(self.ranger.proficiencies))
+
+    def test_saving_throws(self):
+        self.assertEqual(['STR', 'DEX'], self.ranger.saving_throws)
+
+    def test_features(self):
+        self.assertEqual(6, len(self.ranger.features))
+
+    def test_spellcasting(self):
+        self.assertTrue(self.ranger.spellcasting is not None)
+
+    def test_asi(self):
+        self.assertEqual({'STR'}, set(self.ranger.asi.keys()))
